c08e092277b1ed9b687109f966a388b907992384
YosukeSUGIURA <44398746+YosukeSugiura@users.noreply.github.com>
2020-06-19 21:05:25 +0900

    Update demo_main.py

--- demo_main.py
+++ demo_main.py
@@ -7,6 +7,8 @@ from six.moves import range
 import os
 import time
 import numpy as np
+from scipy import stats
+from tqdm import tqdm
 
 #   NNabla
 import nnabla as nn
@@ -23,51 +25,6 @@ from settings import settings
 import data as dt
 
 # -------------------------------------------
-#   Sub Class for Displaying Progress in Console
-# -------------------------------------------
-class display:
-
-    # Remaining Time Estimation
-    class time_estimation:
-
-        def __init__(self, epoch, batch_num):
-            self.start = time.time()
-            self.epoch = epoch
-            self.batch = batch_num
-            self.all = batch_num * epoch
-
-        def __call__(self, epoch_num, batch_num):
-            amount = (batch_num + 1) + epoch_num * self.batch
-            remain = (time.time() - self.start) / amount * (self.all - amount)
-
-            hours, mins = divmod(int(time.time() - self.start), 3600)
-            mins, sec = divmod(mins, 60)
-            hours_e, mins_e = divmod(int(remain), 3600)
-            mins_e, sec_e = divmod(mins_e, 60)
-
-            elapse_time = [int(hours), int(mins), int(sec)]
-            remain_time = [int(hours_e), int(mins_e), int(sec_e)]
-
-            return elapse_time, remain_time
-
-    def __init__(self, epoch, batch_num):
-
-        self.tm = self.time_estimation(epoch, batch_num)
-        self.batch = batch_num
-
-    def __call__(self, epoch, trial, loss):
-
-        elapse_time, remain_time = self.tm(epoch, trial)
-        print('  ---------------------------------------------------')
-        print('  [ Epoch  # {0},    Trials  # {1}/{2} ]'.format(epoch + 1, trial + 1, self.batch))
-        print('    +  Loss                    = {:.4f}'.format(loss))
-        print('    -------------------------')
-        print('    +  Elapsed Time            : {0[0]:3d}h {0[1]:02d}m {0[2]:02d}s'.format(elapse_time))
-        print('    +  Expected Remaining Time : {0[0]:3d}h {0[1]:02d}m {0[2]:02d}s'.format(remain_time))
-        print('  ---------------------------------------------------')
-
-
-# -------------------------------------------
 #   Network for IQA
 # -------------------------------------------
 def network(input, scope="", test=False):
@@ -77,20 +34,43 @@ def network(input, scope="", test=False):
        test:    test or not
     """
 
-    with nn.parameter_scope(scope):
-        c1 = PF.prelu(PF.convolution(input, 10, (8,8), (2,2), (2,2), name='cnv1'))   # Convolution : 64,64,3 -> 2,32,29
-        c2 = F.max_pooling(c1, (2,2), (2,2))                                 # Max Pooling : 2,32,29 -> 2,32,14
-        #c2 = PF.batch_normalization(c2, batch_stat=not test, name='bn1')  # Batch Normalization
-        c4 = PF.prelu(PF.convolution(c2, 16, (8,8), (2,2), (1,1), name='cnv2'))          # Convolution : 2,32,14 -> 16,15,5
-        #c4 = PF.batch_normalization(c4, batch_stat=not test, name='bn2')  # Batch Normalization
-        c6 = F.max_pooling(c4, (2,2), (2,2))                                 # Max Pooling : 16,15,5 -> 16,15,1
-        #c6 = PF.batch_normalization(c5, batch_stat=not test)                        # Batch Normalization
-        c7 = PF.prelu(c6, name='PReLU')                                             # PReLU
-        c8 = PF.affine(c7, (100,), name='Affine')                                   # Affine : 16,15,1 -> 142
-        c9 = F.relu(c8)                                                     # ReLU
-        c10= PF.affine(c9, (1,), name='Affine2')                                    # Affine : 142 -> 1
+    # not used
+    def resblock(x, n=256,name=''):
+        r = x
+        with nn.parameter_scope('block1-'+name):
+            r = PF.convolution(r, n, kernel=(5, 5), pad=(2, 2), stride=(1, 1))
+            r = PF.batch_normalization(r, batch_stat=not test)
+            r = F.relu(r)
+        with nn.parameter_scope('block2-'+name):
+            r = PF.convolution(r, n, kernel=(5, 5), pad=(2, 2), stride=(1, 1))
+        return F.concatenate(r, x, axis=1)
 
-    return c10
+    def convblock(x, n, f_size=9, name=''):
+        r = PF.convolution(x, n, kernel=(f_size, f_size), pad=(f_size//2, f_size//2), stride=(1, 1), name=name)
+        return F.relu(r)
+
+    with nn.parameter_scope(scope):
+
+        # Convolution : 3,64,64 -> 32,64,64 -> 32,32,32
+        c1 = convblock(input, 64, name='cnv1')
+        c1 = F.max_pooling(c1, (2,2), (2,2))
+
+        # Convolution : 32,32,32 -> 32,32,32 -> 32,16,16
+        c2 = convblock(c1, 32, name='cnv2')
+        c2 = F.max_pooling(c2, (2,2), (2,2))
+
+        # Convolution : 32,16,16 -> 64,16,16 -> 64,8,8
+        c3 = convblock(c2, 16, f_size=5, name='cnv3')
+        c3 = F.max_pooling(c3, (2, 2), (2, 2))
+
+        # Convolution : 64,8,8 -> 64,8,8 -> 64,4,4
+        c4 = convblock(c2, 16, f_size=3, name='cnv4')
+        c4 = F.max_pooling(c3, (2, 2), (2, 2))
+
+        c3 = F.leaky_relu(PF.affine(c3, (128,), name='Affine1'), 0.01)
+        c4 = PF.affine(c3, (1,), name='Affine2')
+
+    return c4
 
 
 # -------------------------------------------
@@ -116,11 +96,11 @@ def train(args):
     Output_test = network(Input, scope=Name, test=True)
 
     #   Loss Definition
-    Loss    = F.mean(F.absolute_error(Output, Trues) )       #   Loss Function (Squared Error)
+    Loss    = F.mean(F.absolute_error(Output, Trues) )      #   Loss Function (Squared Error)
     Loss_test = F.mean(F.absolute_error(Output_test, Trues))
 
     #   Solver Setting
-    solver = S.AMSBound(args.learning_rate)                     #   Adam is used for solver
+    solver = S.AMSBound(args.learning_rate)         #   Adam is used for solver
     with nn.parameter_scope(Name):                  #   Get updating parameters included in scope
         solver.set_parameters(nn.get_parameters())
 
@@ -139,48 +119,58 @@ def train(args):
     ##   Learning
     ##  ~~~~~~~~~~~~~~~~~~~
     print('== Start Training ==')
-    disp = display(args.epoch, batches.iter)
+
+    bar = tqdm(total=args.epoch-args.retrain, leave=False)
+    bar.clear()
+    loss_disp = None
+    SRCC = None
 
     #   Load data
     if args.retrain > 0:
         with nn.parameter_scope(Name):
-            print('Retrain from {0} Epoch',format(args.retrain))
+            print('Retrain from {0} Epoch'.format(args.retrain))
             nn.load_parameters(os.path.join(args.model_save_path, "network_param_{:04}.h5".format(args.retrain)))
             solver.set_learning_rate(args.learning_rate/np.sqrt(args.retrain))
 
     ##  Training
     for i in range(args.retrain,args.epoch):
 
-        print('')
-        print(' =========================================================')
-        print('  Epoch :: {0}/{1}'.format(i + 1, args.epoch))
-        print(' =========================================================')
-        print('')
+        bar.set_description_str('Epoch {0}:'.format(i+1), refresh=False)
+        if (loss_disp is not None) and (SRCC is not None):
+            bar.set_postfix_str('Loss={0:.5f},  SRCC={1:.4f}'.format(loss_disp, SRCC), refresh=False)
+        bar.update(1)
+
+        #   Shuffling
+        batches.shuffle()
+        batches_test.shuffle()
 
         ##  Batch iteration
-        for j in range(batches.iter):
+        for j in range(batches.iter_n):
 
             #  Load Batch Data from Training data
-            Input.d, Trues.d    = batches.next(j)
+            Input.d, Trues.d    = next(batches)
 
             #  Update
             solver.zero_grad()                      #   Initialize
             Loss.forward(clear_no_need_grad=True)   #   Forward path
-            Loss.backward(8, clear_buffer=True)     #   Backward path
-            solver.scale_grad(1/8)                  #   Magic spell
-            solver.weight_decay(8*0.001)             #   Weight Decay for stable update
+            Loss.backward(clear_buffer=True)        #   Backward path
+            solver.weight_decay(0.00001)             #   Weight Decay for stable update
             solver.update()
 
         ## Progress
         # Get result for Display
-        Input.d, Trues.d = batches_test.next(i%batches_test.iter)
+        Input.d, Trues.d = next(batches_test)
         Loss_test.forward(clear_no_need_grad=True)
+        Output_test.forward()
+        loss_disp = Loss_test.d
+        SRCC, _ = stats.spearmanr(Output_test.d, Trues.d)
 
         # Display text
-        disp(i, j, Loss_test.d)
+        # disp(i, batches.iter_n, Loss_test.d)
 
         ## Save parameters
         if ((i + 1) % args.model_save_cycle) == 0 or (i + 1) == args.epoch:
+            bar.clear()
             with nn.parameter_scope(Name):
                 nn.save_parameters(os.path.join(args.model_save_path, 'network_param_{:04}.h5'.format(i+1)))
 
@@ -211,26 +201,32 @@ def test(args):
     with nn.parameter_scope(Name):
         nn.load_parameters(os.path.join(args.model_save_path, "network_param_{:04}.h5".format(args.epoch)))
 
-    #   Training Data Setting
+    # Training Data Setting
     image_data, mos_data = dt.data_loader(test=True)
     batches = dt.create_batch(image_data, mos_data, 1)
     del image_data, mos_data
-    
-    #   Test
+
+    truth = []
     result = []
-    start_time = time.time()
-    for j in range(batches.iter):
-        Input.d, Trues.d = batches.next(j)
+    for j in range(batches.iter_n):
+        Input.d, tures = next(batches)
         Loss_test.forward(clear_no_need_grad=True)
         result.append(Loss_test.d)
-    stop_time = time.time()
+        truth.append(tures)
 
-    result_ave = np.average(np.array(result))
-    print('  ---------------------------------------------------')
-    print('  [ Load model trained at Epoch  # {0} ]'.format(args.epoch))
-    print('    +  Averaged Loss           = {:.4f}'.format(result_ave))
-    print('    +  Time per 1 image        = {:.4f} [s]'.format( (stop_time-start_time) / (batches.iter))
-    print('  ---------------------------------------------------')
+    result = np.array(result)
+    truth = np.squeeze(np.array(truth))
+
+    # Evaluation of performance
+    mae = np.average(np.abs(result - truth))
+    SRCC, p1 = stats.spearmanr(truth, result)    # Spearman's Correlation Coefficient
+    PLCC, p2 = stats.pearsonr(truth, result)
+
+    #   Display
+    print("\n Model Parameter [epoch={0}]".format(args.epoch))
+    print(" Mean Absolute Error with Truth: {0:.4f}".format(mae))
+    print(" Speerman's Correlation Coefficient: {0:.3f}".format(SRCC))
+    print(" Pearson's Linear Correlation Coefficient: {0:.3f}".format(PLCC))
 
 
 # -------------------------------------------
@@ -377,7 +373,7 @@ def demo(args):
 if __name__ == '__main__':
 
 
-    Demo = True         # If demo, set "True".
+    Demo = False         # If demo, set "True".
 
     Test = False         # If test, set "True". Otherwise training, set "False".
 
